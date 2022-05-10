281f1b05044b332c7ae456df04036335d679e326
Mominul Islam Rasal <mominul.rasal@bjitgroup.com>
2022-05-10 13:08:36 +0600

    migration file change

--- database/migrations/20220422105242-create-blacklist-jwt.js
+++ database/migrations/20220422105242-create-blacklist-jwt.js
@@ -1,27 +1,27 @@
 'use strict';
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('blacklistJWTs', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      token: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('blacklistJWTs');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('blacklistjwts', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            token: {
+                type: Sequelize.STRING,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('blacklistJWTs');
+    },
 };
